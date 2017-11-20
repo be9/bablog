eb6dfb7c218087b2f536dc94ad9130e67471817d
derokarf <derokarf@gmail.com>
2017-11-20 15:12:16 +0800

    исправлено: - форматирование - Date.prototype - moment.locale

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const posts = require('./routes/posts');
 
 const app = express();
 
+//set date|time locale
+moment.locale('ru');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
